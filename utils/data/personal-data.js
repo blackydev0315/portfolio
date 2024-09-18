9ea6ee0789ebf0b69a9ae9761efcc65480dc0fca
Chris2003 <christianmcdonald990@gmail.com>
2024-09-18 12:02:49 -0500

    Chrish

---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "Christian McDonald",
+  profile: '/profile.png',
+  designation: "Software Developer",
+  description: "Senior Software Engineer with 10 years of experience in full-stack development, specializing in scalable web applications, cloud infrastructure, and improving user engagement. Proven track record in leading crossfunctional teams and delivering high-impact projects in industries like healthcare and e-commerce.",
+  email: 'christianmcdonald990@gmail.com',
+  phone: '+1 (469) 638-2999',
+  address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
+  github: 'https://github.com/said7388',
+  facebook: 'https://www.facebook.com/abusaid.riyaz/',
+  linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
+  twitter: 'https://twitter.com/said7388',
+  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+  leetcode: "https://leetcode.com/said3812/",
+  devUsername: "said7388",
+  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+}
